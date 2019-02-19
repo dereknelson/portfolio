a30ccaca6bb9a8e517a351ad4dcc9d7a11962498
Derek <dereknelsony0@gmail.com>
2019-02-19 15:09:25 -0500

    god help me

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -4,8 +4,8 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": () => import("/Users/derek/portfolio/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-about-js": () => import("/Users/derek/portfolio/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-app-js": () => import("/Users/derek/portfolio/src/pages/App.js" /* webpackChunkName: "component---src-pages-app-js" */),
   "component---src-pages-blog-js": () => import("/Users/derek/portfolio/src/pages/blog.js" /* webpackChunkName: "component---src-pages-blog-js" */),
+  "component---src-pages-index-js": () => import("/Users/derek/portfolio/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-skills-js": () => import("/Users/derek/portfolio/src/pages/skills.js" /* webpackChunkName: "component---src-pages-skills-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,8 +7,8 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/derek/portfolio/.cache/dev-404-page.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/derek/portfolio/src/pages/about.js"))),
-  "component---src-pages-app-js": hot(preferDefault(require("/Users/derek/portfolio/src/pages/App.js"))),
   "component---src-pages-blog-js": hot(preferDefault(require("/Users/derek/portfolio/src/pages/blog.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/derek/portfolio/src/pages/index.js"))),
   "component---src-pages-skills-js": hot(preferDefault(require("/Users/derek/portfolio/src/pages/skills.js")))
 }
 
